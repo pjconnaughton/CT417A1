724fcf04d060cfde7a28053cef44b29694611664
pjconnaughton <p.connaughton4@nuigalway.ie>
2022-10-14 13:40:02 +0100

    module

--- src/main/java/SingleModule.java
+++ src/main/java/SingleModule.java
@@ -1,2 +1,24 @@
+
+import java.util.ArrayList;
+
 public class SingleModule {
+    private String name;
+    private String moduleID;
+    private ArrayList<Student> students;
+    private ArrayList<Course> courses;
+    private Lecturer lecturer;
+
+    public SingleModule(String name, String moduleID, Lecturer lecturer) {
+        this.name = name;
+        this.moduleID = moduleID;
+        this.lecturer = lecturer;
+    }
+
+    public void addStudent(Student s) {
+        students.add(s);
+    }
+
+    public void addCourse(Course c) {
+        courses.add(c);
+    }
 }
